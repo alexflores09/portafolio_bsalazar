08dadf730c95d994435a7e3ce7d0be97d8826bb0
Alexander Flores <alexflores09.06.90@gmail.com>
2017-08-13 13:55:54 -0600

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(()=>{
+    $('ul.tabs').tabs();
+    $('.carousel').carousel();
+
+    $('.responsive-recomendaciones').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
