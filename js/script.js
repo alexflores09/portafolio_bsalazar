0e11a7035f323d48af4052c6814ecd514a819c94
Alexander Flores <alexflores09.06.90@gmail.com>
2017-09-13 01:20:16 -0600

    casi ultimos cambios

--- js/script.js
+++ js/script.js
@@ -1,35 +1,158 @@
 $(document).ready(()=>{
     $('ul.tabs').tabs();
     $('.carousel').carousel();
-
-    $('.responsive-recomendaciones').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
-
     $("#frmContact").on("submit",(e)=>{
         e.preventDefault();
-        alert("aqui tengo que hacer el ajax a contacto")
+        $.ajax({
+            type: "POST",
+            url : "sendEmail.php",
+            data : $("#frmContact").serialize(),
+            dataType : "JSON",
+            success : (data)=>{
+                if(data.valido === 1){
+                    const $toastContent = $(`<span>${data.msj}</span>`);
+                    Materialize.toast($toastContent, 3000);
+                    setTimeout(()=>{
+                        location.reload(false);
+                    },3000);
+                }
+                else{
+                    const $toastContent = $(`<span>${data.msj}</span>`);
+                    Materialize.toast($toastContent, 3000);
+                }
+            }
+        });
     });
+    getLogos();
+    getPortafolio();
 });
+
+function getLogos(){
+    const cont = $(".responsive-recomendaciones");
+    $.ajax({
+        type: "GET",
+        url : "data/logos.txt",
+        dataType : "JSON",
+        beforeSend: ()=>{
+            cont.html("");
+        },
+        success: (data)=>{
+            $.each(data.logos,(key,val)=>{
+                //<div class="center"><img src="img/soporte.jpg"></div>
+
+                let divImage = $("<div></div>").addClass("center");cont.append(divImage);
+                let image = $("<img />").attr({
+                    "src": val,
+                    "alt": "Empresa laborada"
+                }).css("width","100%");divImage.append(image);
+            });
+            $('.responsive-recomendaciones').slick({
+                dots: false,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                    {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false
+                        }
+                    }
+                ]
+            });
+        }
+    });
+}
+
+function getPortafolio(){
+    const contApps = $("#apps");
+    const contImpresion = $("#impresion");
+    const contVectores = $("#vectores");
+    const contWebsite = $("#websites");
+    $.ajax({
+        type: "GET",
+        url : "data/portafolio.txt",
+        dataType : "JSON",
+        beforeSend: ()=>{
+            contApps.html("");
+            contImpresion.html("");
+            contVectores.html("");
+        },
+        success: (data)=>{
+            $.each(data.apps,(key,val)=>{
+                let card = $(getCard(val.img,val.name,val.desc,val.link));
+                contApps.append(card);
+            });
+            $.each(data.revistas,(key,val)=>{
+                let card = $(getCard(val.img,val.name,val.desc,val.link));
+                contImpresion.append(card);
+            });
+            $.each(data.vectores,(key,val)=>{
+                let card = $(getCard(val.img,val.name,val.desc,val.link));
+                contVectores.append(card);
+            });
+            $.each(data.website,(key,val)=>{
+                let card = $(getCard(val.img,val.name,val.desc,val.link));
+                contWebsite.append(card);
+            });
+            $('.materialboxed').materialbox();
+        }
+    });
+}
+
+function getCard(img,name,desc,link){
+
+    let content = "";
+    let style = `style="max-height: 100%;"`;
+    if(name != ""){
+        let description = "";
+        if(desc != ""){
+            description = `
+                <br>
+                ${desc}
+            `;
+        }
+        content = `
+            <div class="card-content">
+                <p>${name} ${description}</p>
+            </div>
+        `;
+        style = `style="max-height: 60%;"`;
+    }
+    let cntLink = "";
+    if(link != ""){
+        cntLink = `
+            <div class="card-action truncate">
+                <a href="${link}" target="_blank">${link}</a>    
+            </div>
+        `;
+        style = `style="max-height: 60%;"`;
+    }
+
+    const template = `
+        <div class="col l3 m4 s12">
+            <div class="card medium">
+                <div class="card-image" ${style}>
+                    <img src="${img}" class="materialboxed" />
+                </div>
+                ${content}
+                ${cntLink}
+            </div>
+        </div>
+    `;
+
+    return template;
+}
---
+++ sendEmail.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex Flores
+ * Date: 13/09/2017
+ * Time: 12:42 AM
+ */
+
+$strNombre = !empty($_REQUEST["name"])?$_REQUEST["name"]:"";
+$strTelefono = !empty($_REQUEST["phone"])?$_REQUEST["phone"]:"";
+$strCorreo = !empty($_REQUEST["email"])?$_REQUEST["email"]:"";
+$strAsunto = !empty($_REQUEST["subject"])?$_REQUEST["subject"]:"Información";
+$strMensaje = !empty($_REQUEST["message"])?$_REQUEST["message"]:"";
+
+
+$headers = "MIME-Version: 1.0";
+$headers .= "\r\n"."Content-type:text/html;charset=UTF-8";
+$headers .= "\r\n"."From: portafoliobsalazar@gmail.com";
+if(!empty($strCorreo))$headers .= "\r\n"."Cc:: {$strCorreo}";
+$headers .= "\r\n"."Bcc: bitcoude@gmail.com";
+
+
+$message = "";
+$message .= <<<EOD
+        <p>
+            <i>
+            “Gracias por su correo electrónico.<br> 
+            En breve estaré contestando su correo.<br>
+            Para obtener asistencia inmediata, por favor póngase en contacto conmigo a mi teléfono celular”.
+            </i>
+        </p>
+
+        <table>
+            <tr>
+                <td>Nombre:</td>
+                <td>{$strNombre}</td>
+            </tr>
+            <tr>
+                <td>Teléfono:</td>
+                <td>{$strTelefono}</td>
+            </tr>
+            <tr>
+                <td>Correo:</td>
+                <td>{$strCorreo}</td>
+            </tr>
+            <tr>
+                <td>Mensaje:</td>
+                <td>{$strMensaje}</td>
+            </tr>
+        </table>
+EOD;
+
+if(@mail("bitcoude@gmail.com",$strAsunto,$message,$headers)){
+    $data["valido"] = 1;
+    $data["msj"] = "Correo enviado correctamente";
+}
+else{
+    $data["valido"] = 0;
+    $data["msj"] = "En este momento no se puede enviar su correo, intente mas tarde.";
+}
+header('Content-Type: application/json');
+echo json_encode($data);
